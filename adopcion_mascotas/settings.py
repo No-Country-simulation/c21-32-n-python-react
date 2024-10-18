71fc4493d9c7ac5832c02a7327a2190b460de231
Kaneshi4744 <santiagogonzalez4744@gmail.com>
2024-10-18 10:33:01 -0500

    commit antes del pull 18/10

--- adopcion_mascotas/settings.py
+++ adopcion_mascotas/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # manually Added 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://localhost:3000"]
 AUTH_USER_MODEL = 'users.Account'
 CORS_ALLOW_CREDENTIALS = True
 
