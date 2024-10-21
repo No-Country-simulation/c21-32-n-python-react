56d2676eff0a71ecc82dd2168ddb0e0ddbdbece2
RassielMex <rhcp94.rm@gmail.com>
2024-10-21 12:51:22 -0600

    Creado Registerform

--- FrontEnd/app/register/page.jsx
+++ FrontEnd/app/register/page.jsx
@@ -1,14 +1,18 @@
+import Image from "next/image";
 import React from "react";
+import RegisterForm from "../ui/register/register-form";
 
 export default function register() {
   return (
     <div className="w-full bg-[#d9d9d9] h-[1245px]">
       <div className="flex justify-between h-full ">
         <div className="w-1/2">
-          <img
+          <Image
             alt="Cat"
             src="/images/cat.jpeg"
-            className="rounded-e-2xl h-full"
+            className="rounded-e-2xl w-full h-full"
+            height={1245}
+            width={700}
           />
         </div>
         <div className="mt-20 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -24,92 +28,7 @@ export default function register() {
               Inicia Sesión
             </a>
           </p>
-          <form action="#" method="POST" className="space-y-6">
-            <div>
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Nombre completo
-              </label>
-              <div className="mt-2">
-                <input
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
-                  id="firstName"
-                  type="text"
-                  required
-                  autoComplete="firstName"
-                />
-              </div>
-            </div>
-            <div>
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Apellido
-              </label>
-              <div className="mt-2">
-                <input
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
-                  id="lastName"
-                  type="text"
-                  required
-                  autoComplete="lastName"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Contraseña
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-gradient-to-r from-red-500 to-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#faa16e] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24AFB5]"
-              >
-                Sign in
-              </button>
-            </div>
-            <div className="text-sm">
-              <a
-                href="#"
-                className="font-semibold text-[#3b83a4] hover:text-[#448aaa]"
-              >
-                ¿Olvidaste tu contraseña?
-              </a>
-            </div>
-          </form>
+          <RegisterForm />
         </div>
       </div>
     </div>
--- FrontEnd/app/ui/register/register-form.jsx
+++ FrontEnd/app/ui/register/register-form.jsx
@@ -1,7 +1,92 @@
+import React from "react";
 
-
-export default function RegisterForm(props) {
+export default function RegisterForm() {
   return (
-              <div></div>
+    <form action="#" method="POST" className="space-y-6">
+      <div>
+        <label className="block text-sm font-medium leading-6 text-gray-900">
+          Nombre completo
+        </label>
+        <div className="mt-2">
+          <input
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
+            id="firstName"
+            type="text"
+            required
+            autoComplete="firstName"
+          />
+        </div>
+      </div>
+      <div>
+        <label className="block text-sm font-medium leading-6 text-gray-900">
+          Apellido
+        </label>
+        <div className="mt-2">
+          <input
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
+            id="lastName"
+            type="text"
+            required
+            autoComplete="lastName"
+          />
+        </div>
+      </div>
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Email
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            required
+            autoComplete="email"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Contraseña
+          </label>
+        </div>
+        <div className="mt-2">
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="current-password"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-gradient-to-r from-red-500 to-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#faa16e] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24AFB5]"
+        >
+          Sign in
+        </button>
+      </div>
+      <div className="text-sm">
+        <a
+          href="#"
+          className="font-semibold text-[#3b83a4] hover:text-[#448aaa]"
+        >
+          ¿Olvidaste tu contraseña?
+        </a>
+      </div>
+    </form>
   );
 }
