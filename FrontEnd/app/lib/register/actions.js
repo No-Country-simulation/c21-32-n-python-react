d0ddc7a5085ff66ff45a9dcc24399d21d27c7a10
RassielMex <rhcp94.rm@gmail.com>
2024-10-22 11:54:15 -0600

    feat: registro conectado al back

---
+++ FrontEnd/app/lib/register/actions.js
@@ -0,0 +1,34 @@
+"use server";
+
+import { logInSchema } from "./schema";
+
+export const registerAction = async (formData) => {
+  try {
+    const dataForm = Object.fromEntries(formData);
+    const validation = logInSchema.safeParse(dataForm);
+    if (validation.success) {
+      const newUser = {
+        ...validation.data,
+        username: validation.data.name,
+        is_active: true,
+        is_admin: false,
+      };
+      const response = await fetch(
+        "http://127.0.0.1:8000/api/v1/email/register/",
+        {
+          method: "POST",
+          body: JSON.stringify(newUser),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response);
+      return { status: response.status, message: response.statusText };
+    }
+    //console.log(validation.error.flatten());
+    return { ...validation.error.flatten() };
+  } catch (error) {
+    console.log(error);
+  }
+};
--- FrontEnd/app/ui/register/register-form.jsx
+++ FrontEnd/app/ui/register/register-form.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+"use client";
+import { registerAction } from "@/app/lib/register/actions";
+import clsx from "clsx";
+import { redirect } from "next/navigation";
+import { useState, useTransition } from "react";
 
 export default function RegisterForm() {
+  const [isPending, startRegister] = useTransition();
+  const [registerResponse, setRegisterResponse] = useState(null);
+
+  const formAction = async (formData) => {
+    startRegister(async () => {
+      const responseAction = await registerAction(formData);
+      setRegisterResponse(responseAction);
+      if (responseAction.status === 201) {
+        redirect("/adoptions");
+      }
+    });
+  };
+
   return (
-    <form action="#" method="POST" className="space-y-6">
+    <form action={formAction} className="space-y-6">
       <div>
         <label className="block text-sm font-medium leading-6 text-gray-900">
           Nombre completo
@@ -10,12 +27,18 @@ export default function RegisterForm() {
         <div className="mt-2">
           <input
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
-            id="firstName"
+            id="name"
+            name="name"
             type="text"
             required
             autoComplete="firstName"
           />
         </div>
+        {registerResponse?.fieldErrors?.name && (
+          <span className="text-xs text-red-800">
+            {registerResponse.fieldErrors.name[0]}
+          </span>
+        )}
       </div>
       <div>
         <label className="block text-sm font-medium leading-6 text-gray-900">
@@ -24,11 +47,17 @@ export default function RegisterForm() {
         <div className="mt-2">
           <input
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
-            id="lastName"
+            id="lastname"
+            name="lastname"
             type="text"
             required
             autoComplete="lastName"
           />
+          {registerResponse?.fieldErrors?.lastname && (
+            <span className="text-xs text-red-800">
+              {registerResponse.fieldErrors.lastname[0]}
+            </span>
+          )}
         </div>
       </div>
       <div>
@@ -48,6 +77,11 @@ export default function RegisterForm() {
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
           />
         </div>
+        {registerResponse?.fieldErrors?.email && (
+          <span className="text-xs text-red-800">
+            {registerResponse.fieldErrors.email[0]}
+          </span>
+        )}
       </div>
 
       <div>
@@ -69,16 +103,28 @@ export default function RegisterForm() {
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-[#207198] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#24AFB5] sm:text-sm sm:leading-6"
           />
         </div>
+        {registerResponse?.fieldErrors?.password && (
+          <span className="text-xs text-red-800">
+            {registerResponse.fieldErrors.password[0]}
+          </span>
+        )}
       </div>
 
       <div>
         <button
+          disabled={isPending}
           type="submit"
-          className="flex w-full justify-center rounded-md bg-gradient-to-r from-red-500 to-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#faa16e] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24AFB5]"
+          className={clsx(
+            "flex w-full justify-center rounded-md bg-gradient-to-r   px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24AFB5]",
+            !isPending && "from-red-500 to-orange-500 hover:bg-[#faa16e] "
+          )}
         >
           Sign in
         </button>
       </div>
+      {registerResponse?.status && registerResponse.status !== 201 && (
+        <span className="text-xs text-red-800">Usuario existente</span>
+      )}
       <div className="text-sm">
         <a
           href="#"
