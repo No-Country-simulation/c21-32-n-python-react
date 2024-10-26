05c9ea7d5e0e104c305199a1abe77f2df005d6fd
RassielMex <rhcp94.rm@gmail.com>
2024-10-26 16:44:28 -0600

    Schema para pets

--- FrontEnd/app/lib/admin/schemas.js
+++ FrontEnd/app/lib/admin/schemas.js
@@ -1,15 +1,50 @@
-import { any, number, object, string } from "zod";
+import { any, nativeEnum, object, string } from "zod";
 
 /*****************PETS*******************/
 
 export const petCreateSchema = object({
-  email: string({ required_error: "Email es requerido" })
-    .min(1, "Email no puede estar vacio")
-    .email("Email invalido"),
-  password: string({ required_error: "Password es requerido" })
-    .min(1, "El password no puede estar vacio")
-    .min(8, "Password debe tener más de 8 caracteres")
-    .max(32, "Password debe tener menos de 32 caracteres"),
+  nombre: string({ required_error: "Nombre es requerido" })
+    .min(1, "Nombre no puede estar vacio")
+    .max(50, "El nombre debe ser menor a 30 caracteres"),
+  tipo: any().refine(
+    (number) => typeof parseInt(number) === "number",
+    "Se necesita un numero valido"
+  ),
+  descripcion: string({ required_error: "Descripcion es requerido" }).min(
+    1,
+    "Descripcion no puede estar vacio"
+  ),
+  raza: string({ required_error: "Raza es requerido" }).min(
+    1,
+    "Raza no puede estar vacio"
+  ),
+  peso: string({ required_error: "Peso es requerido" }).min(
+    1,
+    "Peso no puede estar vacio"
+  ),
+  edad: any().refine(
+    (number) => typeof parseInt(number) === "number",
+    "Se necesita un numero valido"
+  ),
+  sexo: nativeEnum[("F", "M")],
+  img_home_principal: any()
+    .refine((file) => file?.size > 0, "Archivo es requerido")
+    .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+    .refine(
+      (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+      "Only .jpg, .jpeg, .png and .webp formats are supported."
+    ),
+  img_print_mascota: any()
+    .refine((file) => file?.size > 0, "Archivo es requerido")
+    .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+    .refine(
+      (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+      "Only .jpg, .jpeg, .png and .webp formats are supported."
+    ),
+  estatus: string({ required_error: "Estatus es requerido" }).min(
+    1,
+    "Estatus no puede estar vacio"
+  ),
 });
 
 /*****************REFUGES*******************/
