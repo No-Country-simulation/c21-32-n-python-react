4e69b4ef555f7619b78c3d6ad97af481e2e9dcc8
RassielMex <rhcp94.rm@gmail.com>
2024-10-28 15:40:17 -0600

    Fix: Datalist posiblemente null

--- FrontEnd/app/ui/adoption/cardList.jsx
+++ FrontEnd/app/ui/adoption/cardList.jsx
@@ -4,7 +4,7 @@ import Card from "./card";
 const CardList = ({ dataList }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {dataList.map((card) => (
+      {dataList?.map((card) => (
         <Card
           key={card.id_mascota}
           id={card.id_mascota}
