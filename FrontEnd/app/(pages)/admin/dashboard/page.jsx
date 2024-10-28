4546df887b23fd9709318de985aa9e6174bd9d1d
diogenesdmartinez@gmail.com <diogenesdmartinez@gmail.com>
2024-10-28 09:14:33 -0400

    Ajuste de color en boton de registro de refugio

--- FrontEnd/app/(pages)/admin/dashboard/page.jsx
+++ FrontEnd/app/(pages)/admin/dashboard/page.jsx
@@ -4,7 +4,7 @@ import RequestDashboard from "@/app/ui/admin/request-dashboard";
 export default async function AdminDashboardPage() {
   const { requests, totalPages } = await fetchRequestsFiltered();
   return (
-    <div className=" h-[825px] w-full mt-10 ml-2 p-10 bg-black rounded-3xl">
+    <div className=" h-[825px] w-full mt-10 ml-2 p-10 bg-slate-100 rounded-3xl">
       <RequestDashboard requests={requests} totalPages={totalPages} />
     </div>
   );
--- FrontEnd/app/ui/admin/refuges/refuges-create-form.jsx
+++ FrontEnd/app/ui/admin/refuges/refuges-create-form.jsx
@@ -165,7 +165,7 @@ export default function RefugesCreateForm() {
   <div className="flex justify-center items-center mt-5 mb-7">
   <button
     type="submit"
-    className="bg-blue-500 w-1/5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    className="bg-bgButton w-1/5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
   >
     Enviar
   </button>
